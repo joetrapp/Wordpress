7437c023c39e3c926f312dd3b40754d1e75a7b0e
joetrapp <53957627+joetrapp@users.noreply.github.com>
2019-12-15 11:33:20 -0800

    Add files via upload

---
+++ wp-configmyexample.php
@@ -0,0 +1,20 @@
+################################# Force direct file upload (only use for easy setups, no production servers)
+
+define('FS_METHOD', 'direct');
+
+################################# End force direct files upload
+
+
+################################## force site url's for access to website
+
+define('WP_HOME','http://demo.47hops.com');
+define('WP_SITEURL','http://demo.47hops.com');
+
+################################# end force url's
+
+################################# Update urls?
+
+#update urls?
+define('RELOCATE',true);
+
+################################# end update url's
